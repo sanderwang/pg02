90c9e619cf50cf5835eb56de5c25e611742a8770
Rednas <buvms@outlook.com>
2026-09-01 23:39:23 +0000

    AUTOMATED: reconcile changes

--- terraform/weave-gitops/weave-gitops.tf
+++ terraform/weave-gitops/weave-gitops.tf
@@ -1,8 +1,16 @@
-resource "aws_secretsmanager_secret" "weave-gitops-secrets" {
-  for_each = toset([
-    "admin-username",
-    "admin-password-hash",
-  ])
 
-  name = "pg02/kubenuts/weave-gitops/${each.value}"
+resource "aws_secretsmanager_secret" "weave-gitops" {
+  name = "pg02/kubenuts/weave-gitops"
+}
+
+resource "aws_secretsmanager_secret_version" "weave-gitops" {
+  secret_id     = aws_secretsmanager_secret.weave-gitops.id
+  secret_string = jsonencode({
+    admin-username      = "CHANGE_ME"
+    admin-password-hash = "CHANGE_ME"
+  })
+
+  lifecycle {
+    ignore_changes = [secret_string]
+  }
 }
