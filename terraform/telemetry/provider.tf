37266c0cc5670dcc66245de7266eb6eedd3f5491
Rednas <buvms@outlook.com>
2026-09-02 16:01:44 +0000

    AUTOMATED: reconcile changes

---
+++ terraform/telemetry/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    region       = "us-west-2"
+    bucket       = "tf-playground-ny2mimvtuwdotjwg9ne4yg"
+    key          = "kubenutty/telemetry/terraform.tfstate"
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  allowed_account_ids = ["472882997329"]
+  region              = "us-west-2"
+}
---
+++ terraform/telemetry/telemetry.tf
@@ -0,0 +1,52 @@
+
+
+data "aws_eks_cluster" "this" {
+  name = "eks-playground"
+}
+
+data "aws_iam_openid_connect_provider" "this" {
+  url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+data "aws_iam_policy_document" "telemetry-secret-store-trust" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [data.aws_iam_openid_connect_provider.this.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${data.aws_iam_openid_connect_provider.this.url}:sub"
+      values   = ["system:serviceaccount:telemetry:telemetry-secret-store"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${data.aws_iam_openid_connect_provider.this.url}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "telemetry-secret-store-access" {
+  statement {
+    effect    = "Allow"
+    actions   = ["secretsmanager:GetSecretValue", "secretsmanager:DescribeSecret"]
+    resources = ["arn:aws:secretsmanager:us-west-2:472882997329:secret:pg02/telemetry/*"]
+  }
+}
+
+resource "aws_iam_role" "telemetry-secret-store" {
+  name               = "pg02-eso-telemetry"
+  assume_role_policy = data.aws_iam_policy_document.telemetry-secret-store-trust.json
+}
+
+resource "aws_iam_role_policy" "telemetry-secret-store-access" {
+  name   = "secrets-manager-access"
+  role   = aws_iam_role.telemetry-secret-store.id
+  policy = data.aws_iam_policy_document.telemetry-secret-store-access.json
+}
